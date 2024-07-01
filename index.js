74525fae0a52d07aff8c3df8b0def8dadac20091
sandrabanchiri <sandrabanchiri39@gmail.com>
2024-07-01 19:18:40 +0300

    created an index.js file

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+
+
+const companyName ='Scuber';
+let mostProfitableNeighborhood = ('Chelsea');
+let companyCeo = 'Susan Smith';
+
+describe('index.js', function () {
+    describe('mostProfitableNeighborhood', function () {
+        it('is declared as equal to chelsea', function () {
+                expect(mostProfitableNeighborhood).to.equal('Chelsea');
+            })
+            
+        })
+       
+})
+    describe('companyCeo', function () {
+        it('is declared as equal to Susan Smith', function () {
+                expect(companyCeo).to.equal('Susan Smith');
+            })
+            
+        })
+       
+
+
+
+
+
